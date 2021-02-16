9f0007e7b71ecb33588b0a1dd2559b42f3c18dbf
Jami Hannus <t9haja00@student.oamk.fi>
2021-02-16 21:29:09 +0200

    base for register

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const db = require('../server');
 const router = express.Router();
-
-router.post('/' , (req, res)=> {
-    res.send('User is created');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+router.get('/' , (req, res)=> {
+    res.send('Here be register');
 });
 
-
+router.post('/',jsonParser, (req, res) => {
+    const { email, password,firstname,lastname } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const newUser = [
+      email,
+      hashedPassword,
+      firstname,
+      lastname
+    ];
+  
+    db.query('SELECT email FROM users WHERE email= $1 ', [req.body.email])
+      .then(email => {
+        if (email.length > 0) return res.status(400).send('Email already exists');
+  
+        db.query('INSERT INTO users (email, password, firstname,lastname) VALUES ($1, $2, $3, $4) ', newUser)
+          .then(() => res.sendStatus(201))
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+          });
+      })
+      .catch(err => {
+        res.sendStatus(500);
+        console.log(err);
+      });
+  });
 
 module.exports = router;
