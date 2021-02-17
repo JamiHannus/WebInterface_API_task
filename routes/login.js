346c95a7892cf3158767cfb1146feed1d3ba383f
Jami Hannus <t9haja00@student.oamk.fi>
2021-02-17 14:37:42 +0200

    basic  login system

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../server');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const passportHttp = require('passport-http');
 
-router.post('/' , (req, res)=> {
+router.get('/' , (req, res)=> {
     res.send('Here be token giving');
 });
+
+//function to find users password with email then compare 
+//request one with the hashed one from database and see if they match
+passport.use(new passportHttp.BasicStrategy((email, password, done) => {
+
+    db.query('SELECT * FROM users WHERE email=$1', [email])
+      .then(user => {
+
+        if (user.length == 0) return done(null, false);
+        const validPassword = bcrypt.compareSync(password,user[0].password );
+        //will console log true or false
+        console.log(validPassword);
+        if (!validPassword) return done(null, false);
+        done(null, user[0].email);
+      })
+  })
+  );
+  
+  // login a user
+  router.post('/', (req, res, next) => {
+    passport.authenticate('basic', (err, data, info) => {
+      if (err) return next(err);
+      if (!data) return res.status(400).send('Invalid email or password.');
+  
+      console.log(data)
+      res.send(data);
+    })(req, res, next);
+  });
+  
 
 
 module.exports = router;
