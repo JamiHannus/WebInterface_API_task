cd2095cf14e548c859da209dff77d56975b0ce8e
Jami Hannus <t9haja00@student.oamk.fi>
2021-02-18 20:15:51 +0200

    post new item with pictures

--- API/routes/items.js
+++ API/routes/items.js
@@ -1,12 +1,55 @@
 const express = require('express');
+const db = require('../server');
 const router = express.Router();
+const multer  = require('multer')
+const multerUpload = multer({ dest: 'uploads/' })
+
+
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+
+// validation 
+var Validator = require('jsonschema').Validator;
+const userRegister = require('../schemas/newItem.json');
+const { path } = require('dotenv/lib/env-options');
 
 router.get('/' , (req, res)=> {
     res.send('Here is items');
+
+    //some query params
 });
 
-router.get('/:id' , (req, res)=> {
-    res.send('Here is item 1');
+router.post('/' ,jsonParser,multerUpload.array('testFiles', 6), (req, res)=> {
+  const { title, description, category, location,deliverytype, price} = req.body;
+  const images = req.files.map(a=>a.path);
+
+  const newItem = [
+       title,
+        description,
+        category,
+        location,
+        images,
+        deliverytype,
+        price,
+        iduser = 1]
+  
+
+  db.query(
+    "INSERT INTO items (title, description, category, location, images,deliverytype, price, iduser) VALUES ($1, $2, $3, $4, $5:json, $6, $7, $8) RETURNING iditem",
+    newItem)
+    .then((data)  =>
+        db.query("SELECT * FROM items WHERE iditem=($1)", (data[0].iditem))
+        .then((result) => res.send(result))      
+    )
+
+    .catch((err) => {
+      console.log("error ", err);
+      res.sendStatus(500);
+    });
 });
+
+
+
 
 module.exports = router;
